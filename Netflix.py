a8870fad3b63f1710aa537222d40f471f804ba4a
Peter Dorfman <pdorfman@obaachan.cs.utexas.edu>
2010-03-02 21:19:05 -0600

    optimizing probe data read and such

--- Netflix.py
+++ Netflix.py
@@ -26,28 +26,32 @@ def netflix_read () :
     currentRatings = {}
     
     f = open(probePath)
-    probeData = f.readlines()
+    probeData = f.read()
     f.close()
+    probeData = probeData.splitlines()
     
     
     
     for probeLine in probeData:
         i = probeLine.find(":")
         if i > -1:
-            thisFile = moviesDir + "mv_%07d.txt" % (int(probeLine.rstrip(":\n")))
+            probeLineLen = len(probeLine)
+            thisFile = moviesDir + "mv_00" + ((6-probeLineLen)*"0") + probeLine[0:probeLineLen-1] + ".txt"
             f = open(thisFile, 'r')
-            thisFile = f.readlines()
+            thisFile = f.read()
             f.close()
+            thisFile = thisFile.splitlines()
             currentRatings.clear()
             for j in range (1, len(thisFile)) :
-                temp = thisFile[j].split(",")
-                currentRatings[int(temp[0])] = int(temp[1])
+                temp = thisFile[j].partition(",")
+                currentRatings[temp[0]] = ord(temp[2][0])-48
         else:
-            trainingData.append(currentRatings[int(probeLine)])
+            trainingData.append(currentRatings[probeLine])
     
     controlData = len(trainingData)*(3,)
 
     print rmse(tuple(trainingData), controlData)
+
 
 
 # ------------
