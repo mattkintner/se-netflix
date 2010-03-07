92700391b01f47f462664911bd7cba57c9aef0f9
Peter Dorfman <pdorfman@nausicaa.cs.utexas.edu>
2010-03-06 22:51:37 -0600

    updated unit tests

--- TestNetflix.py
+++ TestNetflix.py
@@ -3,8 +3,14 @@
 #     TestNetflix.py
 
 import unittest
-
-from Netflix import netflix_learn, netflix_eval, netflix_print, ratingProfile, custProfile, movieProfile
+import Netflix
+netflix_learn   = Netflix.netflix_learn
+netflix_eval    = Netflix.netflix_eval
+netflix_print   = Netflix.netflix_print
+ratingProfile   = Netflix.ratingProfile
+custProfile     = Netflix.custProfile
+movieProfile    = Netflix.movieProfile
+addMovieRating  = Netflix.addMovieRating
 
 # ------
 # Reader
@@ -56,7 +62,7 @@ class TestNetflix(unittest.TestCase) :
         
         
         
-    def test_addCustRatings (self) :
+    def test_addRating_cust (self) :
         p = custProfile()
         p.addRating(2)
         p.addRating(4)
@@ -70,31 +76,33 @@ class TestNetflix(unittest.TestCase) :
         self.assert_(p.avgRating    == 5)
         self.assert_(p.stdDev       == 2)
 
+    def test_addRating_movie (self) :
+        m = movieProfile()
+        m.addRating(2)
+        m.addRating(4)
+        m.addRating(4)
+        m.addRating(4)
+        m.addRating(5)
+        m.addRating(5)
+        m.addRating(7)
+        m.addRating(9)
+        self.assert_(m.numRated     == 8)
+        self.assert_(m.avgRating    == 5)
+        self.assert_(m.stdDev       == 2)
+
+
+    # ----
+    # add movie ratings to global list
+    # ----
+
     def test_addMovieRatings (self) :
-        p = movieProfile()
-        self.assert_(p.avgRating    == 0)
-        self.assert_(p.numRated     == 0)
-        self.assert_(p.Q            == 0)
-        self.assert_(p.stdDev       == 0)
-        p.addRating(2)
-        p.addRating(4)
-        p.addRating(4)
-        p.addRating(4)
-        p.addRating(5)
-        p.addRating(5)
-        p.addRating(7)
-        p.addRating(9)
-        self.assert_(p.numRated     == 8)
-        self.assert_(p.avgRating    == 5)
-        self.assert_(p.stdDev       == 2)
-
-
-    # ----
-    # rating profiles
-    # ----
-
-    def test_movieRatings (self) :
-        pass
+        self.assert_(len(Netflix.custProfiles) == 0)
+        self.assert_(Netflix.movieProfiles[1] == None);
+        self.assert_(type(Netflix.movieProfiles[1]) is not Netflix.movieProfile);
+        
+        addMovieRating(1,2,3)
+        self.assert_(len(Netflix.custProfiles) == 1)
+        self.assert_(type(Netflix.movieProfiles[1]) is Netflix.movieProfile);
 
 
 if __name__ == "__main__" :
